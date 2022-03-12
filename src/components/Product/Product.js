2eb69a2d52c407461a2715ecf00f8df5c2632162
Tampoka <goal.thanks@gmail.com>
2022-03-12 21:56:43 +0900

    Add products.

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import s from './Product.module.scss';
-import aqua from '../../assets/images/products/image-aqua.png'
-import daniel from '../../assets/images/avatars/daniel.jpg';
 
-export const Product = () => {
+export const Product = (props) => {
     return (
         <div className={s.item}>
             <div className={s.image}>
-                <img src={aqua} alt=""/>
+                <img src={props.productImageUrl} alt=""/>
             </div>
             <div className={s.itemContent}>
                 <div className={s.description}>
-                    <a href="#"> Fort Knight</a>
-                    <p>Authentic renaissance actors, delivered in just two weeks.</p>
+                    <a href={props.url}>{props.title}</a>
+                    <p>{props.description}</p>
                 </div>
                 <div className={s.extra}>
                     <span>Submitted by:</span>
-                    <img className={s.avatarImg} src={daniel} alt="userAvatar"/>
+                    <img className={s.avatarImg} src={props.submitterAvatarUrl} alt="userAvatar"/>
                 </div>
             </div>
         </div>
--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import s from './ProductsList.module.scss'
 import {Product} from '../Product/Product';
+import {products} from '../../assets/data/seed';
 
 export const ProductsList = () => {
+    const product = products[0];
     return (
-        <div>
-            <Product/>
+        <div className={s.productsList}>
+            <Product
+                id={product.id}
+                title={product.title}
+                description={product.description}
+                url={product.url}
+                votes={product.votes}
+                submitterAvatarUrl={product.submitterAvatarUrl}
+                productImageUrl={product.productImageUrl}/>
         </div>
     );
 };
